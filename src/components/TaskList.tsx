7215733ffa24be61884ff6c2115e353bb4e1ec5f
けんし <kensiiwasaki@gmail.com>
2022-05-02 02:10:09 +0900

    fix: TaslList

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+import { useQueryTasks } from '../hooks/useQueryTasks'
+import { Spinner } from './Spinner'
+import { TaskItem } from './TaskItem'
+
+const TaskList: FC = () => {
+  const { data: tasks, status } = useQueryTasks()
+  if (status === 'loading') return <Spinner />
+  if (status === 'error') return <p>{'Error'}</p>
+
+  return (
+    //   受け取ったタスク一覧をmapで画面に表示する
+    <ul className="my-2">
+      {tasks?.map((task) => (
+        <TaskItem key={task.id} id={task.id} title={task.title} />
+      ))}
+    </ul>
+  )
+}
+
+export default TaskList
